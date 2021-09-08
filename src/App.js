37df8277336a3c455defa592ee3d8a71da3836d3
justinx255 <sudharshanraj45@gmail.com>
2021-09-08 20:48:39 +0530

    class

--- src/App.js
+++ src/App.js
@@ -1,86 +1,190 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 
-export default function App() {
-  const [cal, SetCal] = useState('')
-  const [result, SetResult] = useState('')
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const operators = ['+', '-', '*', '/'];
-  const updateDisp = value => {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { cal: '', result: '' };
+  }
+  operators = ['+', '-', '*', '/'];
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  updateDisp = (value) => {
     if (
-      (operators.includes(value) && cal === '') ||
-      (operators.includes(value) && operators.includes(cal.slice(-1)))
-    ) {
-      // console.log('dude')
 
-      return
+      (this.operators.includes(value) && this.state.cal === '') ||
+      (this.operators.includes(value) && this.operators.includes(this.state.cal.slice(-1)))
+    ) {
+      return ''
     }
-    SetCal(cal + value);
-    if (!operators.includes(value)) {
+    console.log(this.operators.includes(this.state.cal.slice(-1)))
+    this.setState({ cal:` ${this.state.cal}  ${value} `})
+    console.log(this.state);
+    
+    if (!this.operators.includes(value)) {
       // eslint-disable-next-line no-eval
-      SetResult(eval(cal + value).toString());
+      let total = eval(this.state.cal + value).toString();
+
+      this.setState = {
+        result: `${total}`
+      };
     }
     // console.log('hi')
   }
-  const calculate = () => {
-    if(cal===''){
+
+  calculate = () => {
+    if (this.state.cal === '') {
       return 0
     }
     // eslint-disable-next-line no-eval
-    SetCal(eval(cal).toString());
+    let tot = eval(this.state.cal).toString();
+    this.setState({
+      result: `${tot}`
+    })
   }
-  return (
-    <div className="App">
-      <div className='calculator'>
-        <div className='display'>
-          {result ? <span>{result}</span> : ''}
-          {
-            cal || "0"
-          }
 
-        </div>
-        <div className='operators'>
-          {operators.map((operator) => {
-            return (
-              <button onClick={() => {
-                updateDisp(operator.toString())
-              }}>{operator}</button>
-            )
-          })}
-          <button onClick={() => {
-            SetCal('');
-            SetResult('')
-          }}>DEL</button>
-        </div>
-        <div className='numbers'>
-          {
-            numbers.map((number) => {
+
+  render() {
+    return (
+      <div className="App">
+        <div className='calculator'>
+          <div className='display'>
+            {this.state.result ? <span>{this.state.result}</span> : ''}
+            {
+              this.state.cal || "0"
+            }
+          </div>
+          <div className='operators'>
+            {this.operators.map((operator) => {
+              return (
+                <button onClick={() => {
+                  this.updateDisp(operator.toString())
+                }}>{operator}</button>
+              )
+            })}
+            <button onClick={() => {
+              this.setState({
+                cal: ''
+              });
+              this.setState({
+                result: ''
+              });
+            }}>DEL</button>
+          </div>
+          <div className='numbers'>
+            {this.numbers.map((number) => {
               return <button onClick={() => {
-                updateDisp(number.toString())
+                this.updateDisp(number.toString())
               }}>{number}</button>
             })
-          }
-          <button onClick={
-            () => {
-              updateDisp('0')
             }
-          }>0</button>
-          <button onClick={
-            () => {
-              updateDisp('.')
-            }
-          }>.</button>
-          <button  className='equal' style={{
-             backgroundColor: "#F8485E",
-             fontWeight:'bold'
-          }} onClick={
-            calculate
-          }>=</button>
-        </div>
+            <button onClick={
+              () => {
+                this.updateDisp('0')
+              }
+            }>0</button>
+            <button onClick={
+              () => {
+                this.updateDisp('.')
+              }
+            }>.</button>
+            <button className='equal' style={{
+              backgroundColor: "#F8485E",
+              fontWeight: 'bold'
+            }} onClick={() => {
+              this.calculate()
+            }}>=</button>
+          </div>
 
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default App;
+
+
+// export default function App() {
+//   const [cal, SetCal] = useState('')
+//   const [result, SetResult] = useState('')
+//   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//   const operators = ['+', '-', '*', '/'];
+//   const updateDisp = value => {
+//     if (
+//       (operators.includes(value) && cal === '') ||
+//       (operators.includes(value) && operators.includes(cal.slice(-1)))
+//     ) {
+//       // console.log('dude')
+
+//       return
+//     }
+//     SetCal(cal + value);
+//     if (!operators.includes(value)) {
+//       // eslint-disable-next-line no-eval
+//       SetResult(eval(cal + value).toString());
+//     }
+//     // console.log('hi')
+//   }
+//   const calculate = () => {
+//     if(cal===''){
+//       return 0
+//     }
+//     // eslint-disable-next-line no-eval
+//     SetCal(eval(cal).toString());
+//   }
+//   return (
+//     <div className="App">
+//       <div className='calculator'>
+//         <div className='display'>
+//           {result ? <span>{result}</span> : ''}
+//           {
+//             cal || "0"
+//           }
+
+//         </div>
+//         <div className='operators'>
+//           {operators.map((operator) => {
+//             return (
+//               <button onClick={() => {
+//                 updateDisp(operator.toString())
+//               }}>{operator}</button>
+//             )
+//           })}
+//           <button onClick={() => {
+//             SetCal('');
+//             SetResult('')
+//           }}>DEL</button>
+//         </div>
+//         <div className='numbers'>
+//           {
+//             numbers.map((number) => {
+//               return <button onClick={() => {
+//                 updateDisp(number.toString())
+//               }}>{number}</button>
+//             })
+//           }
+//           <button onClick={
+//             () => {
+//               updateDisp('0')
+//             }
+//           }>0</button>
+//           <button onClick={
+//             () => {
+//               updateDisp('.')
+//             }
+//           }>.</button>
+//           <button  className='equal' style={{
+//              backgroundColor: "#F8485E",
+//              fontWeight:'bold'
+//           }} onClick={
+//             calculate
+//           }>=</button>
+//         </div>
+
+//       </div>
+//     </div>
+//   );
+// }
 
